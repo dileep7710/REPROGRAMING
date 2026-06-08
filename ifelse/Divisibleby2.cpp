f19afb8d233dbbc993808162d4f3c1be145c634b
Dileep Yadav <dy695354612@gmail.com>
2026-06-08 14:26:51 +0530

    divisible by 2

---
+++ ifelse/Divisibleby2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter you number";
+    cin>>n;
+    if(n%2 == 0){
+        cout<<"Divisible by 2";
+    }
+    else{
+        cout<<"Not Divisible by 2";
+    }
+
+}
