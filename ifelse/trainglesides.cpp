207af49257cd73f705aa0a457dfe2784c44d370f
Dileep Yadav <dy695354612@gmail.com>
2026-06-11 12:34:50 +0530

    Trainglesides

---
+++ ifelse/trainglesides.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter 1st side : ";
+    cin>>a;
+
+    cout<<"Enter 2nd side : ";
+    cin>>b;
+
+    cout<<"Enter 3rd side : ";
+    cin>>c;
+
+    if((a+b>c) && (b+c>a) && (c+a>b)){
+        cout<<a<<","<<b<<","<<c<<" can be the sides of a traingle";
+
+    }
+    else{
+        cout<<"Invalid Triangle";
+    }
+}
