99ce3f5b66c1a256fcd9a4783b4c0262c504c30b
Dileep Yadav <dy695354612@gmail.com>
2026-06-12 16:27:43 +0530

    weekNumber

---
+++ ifelse/weekNumber.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter  day number : ";
+    cin>>n;
+    switch(n){
+        case 1 :
+        cout<<"Sunday";
+            break;
+
+        case 2 : 
+        cout<<"monday";
+             break;
+
+        case 3 : 
+        cout<<"Wednesday";
+             break;
+
+        case 4 :
+        cout<<"Thursday";
+             break;
+
+        case 5 :
+        cout<<"friday";
+             break;
+
+        case 6 : 
+        cout<<"saturday";
+             break;
+
+        case 7 : 
+        cout<<"sunday";
+             break;
+
+        default :
+            cout<<"Invalid number";
+
+
+
+
+    }
+}
