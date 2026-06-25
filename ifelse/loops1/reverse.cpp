e14e3bc30174c11578726c81772b241231540152
Dileep Yadav <dy695354612@gmail.com>
2026-06-25 12:25:45 +0530

    alphabets

---
+++ ifelse/loops1/reverse.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n;
+    // cout<<"Enter a number";
+    // cin>>n;
+    // // n n-1 n-2 .... 3 2 1
+    // for(int i=n; i>=1; i--){
+    //     cout<<i<<endl;
+    // }
+
+    // 100 97  94 ..... 7 4 1 -> 34 terms
+    
+    for(int a=100; a>0; a-=3){
+        cout<<a<<endl;
+        
+    }
+}
---
+++ ifelse/loops1/while.cpp
@@ -0,0 +1,21 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i=1;
+//     while(i<= 10){
+//         cout<<i<<endl;
+//         i++;
+//     }
+    
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i = 11;
+    do{
+        cout<<i<<endl;
+        i++;
+    }
+    while(i<=10);
+} 
