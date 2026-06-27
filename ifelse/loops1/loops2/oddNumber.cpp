e3f0882b61d782616e5d012364ad9dc2051455da
Dileep Yadav <dy695354612@gmail.com>
2026-06-27 14:40:41 +0530

    count

---
+++ ifelse/loops1/loops2/oddNumber.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1; i<=100; i++){
+//         if(i%2==0) continue;
+//         cout<<i<<endl;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int t=10;
+//     while(t/=2){
+//         cout<<"Hello"<<endl;
+//     }
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    while(i=10){
+        cout<<i<<endl;
+        i=i+1;
+    }
+}
