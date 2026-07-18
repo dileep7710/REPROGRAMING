d15a2e8908ff4f88c754c56908b106e5e47ea74d
Dileep Yadav <dy695354612@gmail.com>
2026-07-18 14:06:52 +0530

    compositeNumber

--- ifelse/loops1/loops2/compositeNumber.cpp
+++ ifelse/loops1/loops2/compositeNumber.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter a number :-";
+    cout<<"enter a number : ";
     cin>>n;
-    bool flag = true; // true means prime
-    for(int i=2; i<=n-1; i++){
-        if(n%i==0){   // composite number
-           flag = false;
+    bool flag = true;
+    for(int i=2; i<n; i++){
+        if(n%i==0){
+            flag = false;
             break;
         }
     }
-    if(n==1) cout<<"1 is neither prime nor composite";
-    if(flag == true) cout<<"It is prime number";
-    else cout<<n<<"-"<<" is Composite number"<<endl;
+     if(flag==true) cout<<"Prime Number";
+     else cout<<n<<"is composite";
 }
