7dc58cb9ed76a6d10c059164ab36f4707b6d45c7
Dileep Yadav <dy695354612@gmail.com>
2026-07-18 16:40:20 +0530

    reverseNumber

---
+++ ifelse/loops1/loops2/someOfdigits.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter a number :";
+    cin>>n;
+    int sum = 0;
+    while(n>0){
+        int lastdigit = n%10;
+        sum = sum+lastdigit; 
+        n=n/10;
+    }
+    cout<<sum;
+}
