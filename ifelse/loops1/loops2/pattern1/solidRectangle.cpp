56340fddcc6ebee0de12bb451c504d2d44f53f29
Dileep Yadav <dy695354612@gmail.com>
2026-07-19 15:46:55 +0530

    pattern printing

---
+++ ifelse/loops1/loops2/pattern1/solidRectangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"No of Rows: ";
+    cin>>n;
+
+    int m;
+    cout<<"No of columns: ";
+    cin>>m;
+    for(int i=1; i<=n; i++){
+        for(int i=1; i<=m; i++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
