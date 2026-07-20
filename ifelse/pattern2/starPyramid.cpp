cce68640e2734fac219ea20692fd0881b3cd203f
Dileep Yadav <dy695354612@gmail.com>
2026-07-20 19:10:08 +0530

    starPyramid

--- ifelse/pattern2/starPyramid.cpp
+++ ifelse/pattern2/starPyramid.cpp
@@ -1,16 +1,41 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a Number : ";
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n-i; j++){
+//             cout<<" ";
+//         }
+//         for(int k=1; k<=2*i-1; k++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter a Number : ";
+    cout<<"Enter a number :";
     cin>>n;
+//     *
+//    ***
+//   *****
+    int nsp = n-1;
+    int nst = 1;
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=n-i; j++){
+        for(int j=1; j<=nsp; j++){
             cout<<" ";
         }
-        for(int k=1; k<=2*i-1; k++){
+        nsp--;
+
+        for(int k=1; k<=nst; k++){
             cout<<"*";
         }
+        nst+=2;
         cout<<endl;
     }
 }
