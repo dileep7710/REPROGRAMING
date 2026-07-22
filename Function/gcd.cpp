d8f42b3db9cbecfa70ad225f3556a8ec7ef50d01
Dileep Yadav <dy695354612@gmail.com>
2026-07-22 16:40:40 +0530

    gcd

---
+++ Function/gcd.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b){
+    int hcf = 1;
+    for(int i=min(a,b); i>=1; i--){
+        if(a%i==0 && b%i==0){
+            hcf = i;
+            break;
+        }
+    }
+    return hcf;
+
+}
+int main(){
+    int a;
+    cout<<"Enter a 1st number : ";
+    cin>>a;
+
+    int b;
+    cout<<"Enter a 2nd number : ";
+    cin>>b;
+
+    cout<<gcd(a,b);
+}
