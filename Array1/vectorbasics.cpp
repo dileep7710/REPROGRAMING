d427d59ff6687a5cd904099e6bc61d2f3a513c6c
Dileep Yadav <dy695354612@gmail.com>
2026-07-25 14:13:04 +0530

    vector

---
+++ Array1/vectorbasics.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v; // you need not mention the size
+    // inserting 
+    v.push_back(6);
+    cout<<v.size()<<endl;
+    v.push_back(1);
+    cout<<v.size()<<endl;
+    v.push_back(9);
+    cout<<v.size()<<endl;
+    v.push_back(10);
+    cout<<v.size()<<endl;
+
+}
