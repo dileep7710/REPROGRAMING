6625b0c870aa385365c5b55a9f7fc5ac636de413
Dileep Yadav <dy695354612@gmail.com>
2026-08-04 19:08:27 +0530

    downWaveprint1

---
+++ 2dArray2/downwavePrint1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter no of row : ";
+    cin>>m;
+
+    int n;
+    cout<<"Enter no of columns : ";
+    cin>>n;
+
+    int arr[m][n];
+    cout<<"Enter no elements : ";
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+
+    for(int i=m-1; i>=0; i--){
+        if(i%2==0){
+            for(int j=0; j<n; j++){
+                cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        else{
+             for(int j=n-1; j>=0; j--){
+            cout<<arr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+       
+    }
+
+}
