6df158ad84b44911e476e135cc2f427fb8b627bb
Dileep Yadav <dy695354612@gmail.com>
2026-08-04 19:41:16 +0530

    coloumnswisePrint

---
+++ 2dArray2/coloumnswiseprint.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"enter no of row : ";
+    cin>>m;
+
+    int n;
+    cout<<"Enter no of columns";
+    cin>>n;
+
+    int arr[m][n];
+    cout<<"Enter no elements :";
+    for(int i=0; i<m; i++){
+        for(int j =0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+
+    for(int j=0; j<n; j++){
+        if(j%2==0){
+            for(int i=0; i<m; i++){
+                cout<<arr[i][j]<<" ";
+            }
+            
+        }
+        else{
+                for(int i= m-1; i>=0; i--){
+                    cout<<arr[i][j]<<" ";
+                }
+            }
+       
+    }
+
+    
+        
+        
+    } 
+
+
+
