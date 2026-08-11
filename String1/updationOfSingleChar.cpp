a5f04500d9cd6370f40b4ea3482b77ec70fa2257
Dileep Yadav <dy695354612@gmail.com>
2026-08-11 17:49:33 +0530

    updation

--- String1/updationOfSingleChar.cpp
+++ String1/updationOfSingleChar.cpp
@@ -2,7 +2,16 @@
 #include<string>
 using namespace std;
 int main(){
-    string str ="Dileep";
-    str[0] = 'o';
-    cout<<str;
+     string str = "Dileep";
+     cout<<str<<endl;
+    // str[0] = 'o';
+    // cout<<str;
+
+    for(int i=0; str[i]!='\0'; i++){
+        if(i%2==0) str[i] = 'a';
+    }
+
+    cout<<str<<endl;
+
+
 }
