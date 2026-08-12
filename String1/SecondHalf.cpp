f2da957f7924d066040dd19fdc15e5bd3c17b073
Dileep Yadav <dy695354612@gmail.com>
2026-08-12 16:30:06 +0530

    string

---
+++ String1/SecondHalf.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s;
+    getline(cin,s);
+    int n = s.length();
+    cout<<s.substr(n/2);
+}
