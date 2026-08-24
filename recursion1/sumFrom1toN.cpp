d8d853aa1c4ba4b1fcec7b4cadf3d215ce195aea
Dileep Yadav <dy695354612@gmail.com>
2026-08-24 17:01:34 +0530

    sumFromtoN

--- recursion1/sumFrom1toN.cpp
+++ recursion1/sumFrom1toN.cpp
@@ -7,7 +7,13 @@ void sum1toN(int sum, int n){
     }
     sum1toN(sum+n,n-1);
 }
+int sum2(int n){
+    if(n==0) return 0;
+    return n + sum2(n-1);
+}
 
 int main(){
-    sum1toN(0,10);
+    //sum1toN(0,10);
+    cout<<sum2(6);
+
 }
