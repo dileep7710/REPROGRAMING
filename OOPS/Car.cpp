75d63339b905679f547574539e0aa5b442f1de72
Dileep Yadav <dy695354612@gmail.com>
2026-09-23 08:26:48 +0530

    constructor

--- OOPS/Car.cpp
+++ OOPS/Car.cpp
@@ -3,9 +3,16 @@ using namespace std;
 class Car{
     public:
     string name;
-    int price;
+    int price; 
     int seats;
     string type;
+
+    Car (string c, int p, int t, string k){
+        name = c;
+        price = p;
+        seats = t;
+        type = k; 
+    }
 
 };
  void print(Car c){
@@ -13,27 +20,30 @@ class Car{
  } 
 
 int main(){
-    Car c1;
-    c1.name = "Honda City";
-    c1.price = 1500000;
-    c1.seats = 5;
-    c1.type = "sedan";
 
-    Car c2;
-    c2.name = "Maruti Swift";
-    c2.price = 700000;
-    c2.seats = 5;
-    c2.type = "Hatchback";
+    Car c1  ("Honda city", 1500000, 5, "sedan");
+    // Car c1;
+    // c1.name = "Honda City";
+    // c1.price = 1500000;
+    // c1.seats = 5;
+    // c1.type = "sedan";
 
-     Car c3;
-    c3.name = "Toyota Fortuner";
-    c3.price = 3600000;
-    c3.seats = 8;
-    c3.type = "SUV";  
+    // Car c2;
+    // c2.name = "Maruti Swift";
+    // c2.price = 700000;
+    // c2.seats = 5;
+    // c2.type = "Hatchback";
 
-    print(c1);
-    print(c2);
-    print(c3);
+    //  Car c3;
+    // c3.name = "Toyota Fortuner";
+    // c3.price = 3600000;
+    // c3.seats = 8;
+    // c3.type = "SUV";  
 
+    // print(c1);
+    // print(c2);
+    // print(c3);
+
+    cout<<c1.name<<" "<<c1.price<<" "<<c1.seats<<" "<<c1.type<<" "<<endl;
 
 }
--- OOPS/constructors.cpp
+++ OOPS/constructors.cpp
@@ -6,20 +6,40 @@ class Student{
     int rno;
     float gpa;
 
-    Student (string s, int r){
-        name = s;
-        rno = r;
+    Student(){ // Default Constructor
 
     }
+
+    // constructors
+    Student (string s, int r){ // parameterised constructor  
+        name = s;
+        rno = r;
+    }
+
+     Student (string s, int r, float g){ // parameterised constructor  
+        name = s;
+        rno = r;
+        gpa = g;
+    }
+
+
 };
 int main(){
     Student s1("Dileep Yadav", 76);
+    s1.gpa = 6.2;
 
-    s1.gpa = 8.2;
+    Student s2;
+    s2.name = "Himanshu";
+    s2.rno = 88;
+    s2.gpa = 8.2;
 
-   // Student s2;
+    Student s3("Anant", 13, 7.2);
+
+    Student s6 = s1;
 
 
     cout<<s1.name<<" "<<s1.gpa<<" "<<s1.rno<<endl;
-    //cout<<s2.name<<" "<<s2.gpa<<" "<<s2.rno<<endl;
+    cout<<s2.name<<" "<<s2.gpa<<" "<<s2.rno<<endl;
+    cout<<s3.name<<" "<<s3.gpa<<" "<<s3.rno<<endl;
+     cout<<s6.name<<" "<<s6.gpa<<" "<<s6.rno<<endl;
 }
